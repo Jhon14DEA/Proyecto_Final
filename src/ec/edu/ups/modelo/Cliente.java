60fe6ea9ef419b8d9808dbda6ec5035b72ba4b51
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-22 18:09:24 -0500

    atributos de la clase Cliente, getter y setter, metodos equals, to String y hash code. Herenda atributos de la clase Usuario

--- src/ec/edu/ups/modelo/Cliente.java
+++ src/ec/edu/ups/modelo/Cliente.java
@@ -5,10 +5,87 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author Dutan2000
  */
-public class Cliente {
+public class Cliente extends Usuario{
+    
+    private String nombreDelLocal;
+    private String telefono;
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombreDelLocal, String telefono, String direccion) {
+        this.nombreDelLocal = nombreDelLocal;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public Cliente(String nombreDelLocal, String telefono, String direccion, String cedula, String nombre, String apellido, String correo, String contraseña) {
+        super(cedula, nombre, apellido, correo, contraseña);
+        this.nombreDelLocal = nombreDelLocal;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombreDelLocal() {
+        return nombreDelLocal;
+    }
+
+    public void setNombreDelLocal(String nombreDelLocal) {
+        this.nombreDelLocal = nombreDelLocal;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.nombreDelLocal);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nombreDelLocal, other.nombreDelLocal)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombreDelLocal=" + nombreDelLocal + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
+    
     
 }
