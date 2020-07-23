a50ec638c75b37a3c116e45ecbae05195c0fecdc
JHON FAREZ <JHON FAREZ@DESKTOP-I45IDQE>
2020-07-22 19:52:18 -0500

    Implementaciones extras en la el paquete DAO

--- src/ec/edu/ups/DAO/UsuarioDAO.java
+++ src/ec/edu/ups/DAO/UsuarioDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.ups.dao;
+package ec.edu.ups.DAO;
 
 /**
  *
  * @author JHON FAREZ
  */
-public class BodegaDAO {
+public class UsuarioDAO {
     
 }
--- src/ec/edu/ups/IDAO/IBodegaDAO.java
+++ src/ec/edu/ups/IDAO/IBodegaDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.ups.dao;
+package ec.edu.ups.idao;
 
 /**
  *
  * @author JHON FAREZ
  */
-public class BodegaDAO {
+public interface IBodegaDAO {
     
 }
