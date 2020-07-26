41fa1540e0a818f38111ea3f5d3ad15e081cdcb9
Santiago-cabrera-Arias <santiagocabrera354@gmail.com>
2020-07-25 20:17:30 -0500

    Creacion del controlador Usuario

---
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -0,0 +1,91 @@
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.DAO.UsuarioDAO;
+import ec.edu.ups.IDAO.IUsuario;
+import ec.edu.ups.modelo.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author santi
+ */
+public class ControladorUsuario {
+
+    private Usuario usuario;
+    private IUsuario usuarioDAO;
+
+    public ControladorUsuario() {
+    }
+
+    public ControladorUsuario(Usuario usuario, UsuarioDAO usuarioDAO) {
+        this.usuario = usuario;
+        this.usuarioDAO = usuarioDAO;
+    }
+
+      public void registrarUsuario(String cedula, String nombre, String apellido, String correo, String contrasena) {
+
+        usuario = new Usuario(cedula, nombre, apellido, correo, contrasena);
+        usuarioDAO.create(usuario);
+
+    }
+
+    public Usuario buscarUsuario(String cedula) {
+
+         usuario = usuarioDAO.read(cedula);
+        
+        if(usuario != null){
+            return usuario;
+        }else{
+            return null;
+        }   
+
+    }
+
+    public boolean validarUsuario(String correo, String contrasena) {
+
+        usuario = usuarioDAO.login(correo, contrasena);
+        if (usuario != null) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
+
+    public void actualizarUsuario(String cedula) {
+
+        usuario = usuarioDAO.read(cedula);
+
+        if (usuario != null) {
+            usuarioDAO.update(usuario);
+        }
+    }
+
+    public boolean eliminarUsuario(String cedula) {
+
+        usuario = usuarioDAO.read(cedula);
+
+        if (usuario != null) {
+            usuarioDAO.delete(usuario);
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public List<Usuario> listarUsuario() {
+
+        List<Usuario> lista = usuarioDAO.findAll();
+        return lista;
+
+    }
+
+}
