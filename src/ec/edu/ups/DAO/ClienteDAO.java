fa9fa45fb827f8b61f5d565916796b1b1895d5aa
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-25 23:07:37 -0500

    Documentacion de la clase ClienteDAO

--- src/ec/edu/ups/DAO/ClienteDAO.java
+++ src/ec/edu/ups/DAO/ClienteDAO.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Clase ClienteDAO.
+ * 
+ * Esta clase tiene el proposito de realizar el manejo de informacion en el archivo
+ * binario, en el cual se sobrescribe los metodos de la clase interfaz ICliente.
+ * 
  * @author Sebastian Uyaguari
  * @author Denys Dutan
  * @author John Fárez
@@ -21,17 +26,29 @@ import java.util.List;
  */
 public class ClienteDAO implements ICliente {
 
+    //Metodos de la clase
     private RandomAccessFile archivo;
     private int tamañoRegistro;
 
     /**
      * Estructura del archivo.
      *
-     * private String cedula = 10 bytes private String nombre = 25 bytes private
-     * String apellido = 25 bytes private String nombreDelLocal 25 bytes private
-     * String telefono = 25 bytes private String direccion = 50 bytes
+     * private String cedula = 10 bytes private 
+     * String nombre = 25 bytes 
+     * private String apellido = 25 bytes 
+     * private String nombreDelLocal 25 bytes 
+     * private String telefono = 25 bytes 
+     * private String direccion = 50 bytes
      *
      * Total bytes registro = 172 bytes
+     */
+    
+    /**
+     * Metodo ClienteDAO.
+     * 
+     * Metodo constructor en el cual se instancia el RandomAccessFile para leer o 
+     * escribir informacion en un archivo.
+     * 
      */
     public ClienteDAO() {
         tamañoRegistro = 172;
@@ -43,6 +60,14 @@ public class ClienteDAO implements ICliente {
         }
     }
 
+    /**
+     * Metodo Create.
+     * 
+     * Metodo sobrescrio de la clase ICliente con el fin de escribir o ingresar
+     * la informacion recibida como parametro en el archivo binario.
+     * 
+     * @param cliente Cliente
+     */
     @Override
     public void create(Cliente cliente) {
         try {
@@ -61,7 +86,17 @@ public class ClienteDAO implements ICliente {
 
         }
     }
-
+    
+    /**
+     * Metodo Read.
+     * 
+     * Metodo sobrescrito de la clase ICliente donde se busca y retorna la 
+     * informacion que este ligada a la id, comparando los datos guardados en el 
+     * archivo con el parametro recibido.
+     * 
+     * @param nombreDelLocal String
+     * @return Cliente:cliente
+     */
     @Override
     public Cliente read(String nombreDelLocal) {
         try {
@@ -83,6 +118,16 @@ public class ClienteDAO implements ICliente {
         return null;
     }
 
+    /**
+     * Metodo Update.
+     * 
+     * Metodo sobrescrito de la clase ICliente, recibe un parametro de tipo
+     * cliente, donde comparamos el id del parametro con los id recuperados
+     * del archivo para poder acualizar la informacion que este ligada al id
+     * del cliente.
+     * 
+     * @param cliente Cliente
+     */
     @Override
     public void update(Cliente cliente) {
         int salto = 0;
@@ -107,7 +152,16 @@ public class ClienteDAO implements ICliente {
             ex.printStackTrace();
         }
     }
-
+    
+    /**
+     * Metodo Delete.
+     * 
+     * Metodo sobrescrito de la clase ICliente, busca la informacion que este 
+     * ligada al id para despues remplazar con espacios vacios realizando una 
+     * eliminacion logica.
+     * 
+     * @param codigo String
+     */
     @Override
     public void delete(String codigo) {
         String cadena = "";
@@ -134,6 +188,13 @@ public class ClienteDAO implements ICliente {
         }
     }
 
+    /**
+     * Metodo findAllClientes.
+     * 
+     * Metodo sobrescrito de la clase ICLiente, recupera toda la informacion guardada
+     * en el archivo para guardar en una lista para ser retornada.
+     * @return List:clienteLista
+     */
     @Override
     public List<Cliente> findAllClientes() {
         List<Cliente> clienteLista = new ArrayList<>();
