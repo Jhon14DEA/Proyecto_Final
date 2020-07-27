3fcd027eabe29d0bb9675fa2e16c7c8206321848
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-26 23:50:41 -0500

    Correccion ControladorCliente

--- src/ec/edu/ups/controlador/ControladorCliente.java
+++ src/ec/edu/ups/controlador/ControladorCliente.java
@@ -11,6 +11,11 @@ import ec.edu.ups.modelo.Cliente;
 import java.util.List;
 
 /**
+ * Clase ControladorCliente.
+ * 
+ * Esta clase realiza como intermediario entre la vista, el modelo y el dao
+ * para manejar la informacion ingresada.
+ * 
  * @author Sebastian Uyaguari
  * @author Denys Dutan
  * @author John Fárez
@@ -18,17 +23,42 @@ import java.util.List;
  */
 public class ControladorCliente {
     
+    //Atributos de la clase
     private Cliente cliente;
     private ICliente clienteDao;
 
+    /**
+     * Metodo ControladorCliente.
+     * 
+     * Metodo constructor vacio.
+     */
     public ControladorCliente() {
     }
 
-    public ControladorCliente(Cliente cliente, ClienteDAO clienteDao) {
-        this.cliente = cliente;
+    /**
+     * MetodoControladorCliente.
+     * 
+     * Metodo constructor en el cual se recibe como paramerto el clienteDao.
+     * 
+     * @param clienteDao: ClienteDAO
+     */
+    public ControladorCliente(ClienteDAO clienteDao) {
         this.clienteDao = clienteDao;
     }
     
+    /**
+     * Metodo crearCLiente.
+     * 
+     * Este metodo recibe la informacion mediante parametros para crear un nuevo
+     * objeto tipo cliente y enviar al dao para registrar en el archivo.
+     * 
+     * @param nombreLocal String
+     * @param telefono String
+     * @param direccion String
+     * @param cedula String
+     * @param nombre String
+     * @param apellido String
+     */
     public void crearCliente(String nombreLocal, String telefono, String direccion, String cedula, String nombre, String apellido){
         
         cliente = new Cliente(nombreLocal, telefono, direccion, cedula, nombre, apellido);
@@ -37,6 +67,13 @@ public class ControladorCliente {
         
     }
     
+    /**
+     * Metodo buscarCliente.
+     * 
+     * Recibe un atributo tipo string para enviar al 
+     * @param nombreLocal String
+     * @return Cliente:cliente
+     */
     public Cliente buscarCliente(String nombreLocal){
         
         cliente = clienteDao.read(nombreLocal);
